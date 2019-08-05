d77b15b585c719872c41c5ad47a01cc1cc7559d2
vijayaraj m s <vijayaraj m s@192.168.1.29>
2019-08-05 17:02:45 +0530

    category got some changes

--- project1backend/src/main/java/com/niit/models/Category.java
+++ project1backend/src/main/java/com/niit/models/Category.java
@@ -1,14 +1,16 @@
  package com.niit.models;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table
-
 public class Category 
 {
 	@Id
@@ -20,19 +22,28 @@ int CategoryId;
 	public void setCategoryId(int categoryId) {
 		CategoryId = categoryId;
 	}
-	public String getCategoryName() {
-		return CategoryName;
-	}
-	public void setCategoryName(String categoryName) {
-		CategoryName = categoryName;
-	}
+	
 	public String getCategoryDesc() {
 		return CategoryDesc;
 	}
 	public void setCategoryDesc(String categoryDesc) {
 		CategoryDesc = categoryDesc;
 	}
-	String CategoryName;
+	@OneToMany(mappedBy="category")
+    private List<Product>products; 
+	public List<Product> getProducts() {
+		return products;
+	}
+	public void setProducts(List<Product> products) {
+		this.products = products;
+	}
+	public String getCategoryname() {
+		return Categoryname;
+	}
+	public void setCategoryname(String categoryname) {
+		Categoryname = categoryname;
+	}
+	String Categoryname;
 	String CategoryDesc;	
 
 }
